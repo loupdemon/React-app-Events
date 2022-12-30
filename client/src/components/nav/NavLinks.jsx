eabb3d6d3d1d91886b865ccfd0d358cc5eff14b0
Šarūnas Grabauskas <sarunas.grabauskas@gmail.com>
2022-12-30 09:35:14 +0200

    fixed spacing and behavior of mobile nav

--- client/src/components/nav/NavLinks.jsx
+++ client/src/components/nav/NavLinks.jsx
@@ -1,41 +1,100 @@
 import React from 'react';
-import { Button } from '@mui/material';
+import { Button, Box } from '@mui/material';
 import LogoutIcon from '@mui/icons-material/Logout';
 import { useNavigate, Link } from 'react-router-dom';
 
-export default function NavLinks() {
+export default function NavLinks(props) {
     const navigate = useNavigate();
     const onUserLogout = () => {
         localStorage.clear();
         navigate('/login');
     };
-    return (
-        <>
-            <Button color='inherit'>
-                <Link
-                    to='/home'
-                    style={{
-                        textDecoration: 'none',
-                        color: 'inherit',
-                    }}
+    if (props.open === true) {
+        return (
+            <>
+                <Button
+                    sx={{ mr: 5, pl: 2 }}
+                    color='inherit'
+                    justify='flex-start'
+                    style={{ justifyContent: 'flex-start' }}
                 >
-                    Events
-                </Link>
-            </Button>
-            <Button color='inherit'>
-                <Link
-                    to='/add_event'
-                    style={{
-                        textDecoration: 'none',
-                        color: 'inherit',
-                    }}
+                    <Link
+                        sx={{ mr: 15 }}
+                        to='/home'
+                        style={{
+                            textDecoration: 'none',
+                            color: 'inherit',
+                        }}
+                    >
+                        Events
+                    </Link>
+                </Button>
+                <Button
+                    color='inherit'
+                    style={{ justifyContent: 'flex-start' }}
+                    sx={{ mr: 5, pl: 2 }}
                 >
-                    Add Event
-                </Link>
-            </Button>
-            <Button onClick={onUserLogout} color='inherit'>
-                <LogoutIcon />
-            </Button>
-        </>
-    );
+                    <Link
+                        to='/add_event'
+                        style={{
+                            textDecoration: 'none',
+                            color: 'inherit',
+                        }}
+                    >
+                        Add Event
+                    </Link>
+                </Button>
+                <Button
+                    sx={{ mr: 5, pl: 2 }}
+                    onClick={onUserLogout}
+                    color='inherit'
+                    style={{ justifyContent: 'flex-start' }}
+                >
+                    <LogoutIcon />
+                </Button>
+            </>
+        );
+    } else {
+        return (
+            <>
+                <Button
+                    color='inherit'
+                    justify='flex-start'
+                    style={{ justifyContent: 'flex-start' }}
+                >
+                    <Link
+                        sx={{ mr: 15 }}
+                        to='/home'
+                        style={{
+                            textDecoration: 'none',
+                            color: 'inherit',
+                        }}
+                    >
+                        Events
+                    </Link>
+                </Button>
+                <Button
+                    color='inherit'
+                    style={{ justifyContent: 'flex-start' }}
+                >
+                    <Link
+                        to='/add_event'
+                        style={{
+                            textDecoration: 'none',
+                            color: 'inherit',
+                        }}
+                    >
+                        Add Event
+                    </Link>
+                </Button>
+                <Button
+                    onClick={onUserLogout}
+                    color='inherit'
+                    style={{ justifyContent: 'flex-start' }}
+                >
+                    <LogoutIcon />
+                </Button>
+            </>
+        );
+    }
 }
